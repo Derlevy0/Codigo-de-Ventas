c4e07b540ebe048d53df884553e56ffde1016617
Derlevy0 <163205452+Derlevy0@users.noreply.github.com>
2024-03-27 18:47:01 -0500

    Agrego contador y empiezo a crear el menu para selecionar las compras

--- Trabajo GitHub (Derlevy).py
+++ Trabajo GitHub (Derlevy).py
@@ -26,6 +26,30 @@ camisetas = {1: ['Polo','Blanca', 15], 2:['Polo','Azul', 15], 3: ['Polo','Roja',
 jean = {1: ['Azul', 20], 2:['Verde', 20], 3:['Café', 20], 4: ['Negro', 20], 5: ['Gris', 20]}
 
 zapatos = {1:['Botas','Café', 25], 2: ['Tenis','Azul', 20], 3:['Botas','Negro', 25], 4: ['Tenis','Blanco', 20]}
+
+Articulos=0
+while True:
+   P = int(input("""Seleccione una Camiseta:
+             1- camisetas[1]
+             2- camisetas[2]
+             3- camisetas[3]
+             4- camisetas[4]
+             5- camisetas[5]
+             6- camisetas[6]
+             7- camisetas[7]"""))
+   
+   S = int(input("""Seleccione un Jean:
+                 1- jean[1]
+                 2- jean[2]
+                 3- jean[3]
+                 4- jean[4]
+                 5- jean[5]"""))
+                 
+   T = int(input("""seleccione unos Zapatos:
+                 1- zapatos[1]
+                 2- zapatos[2]
+                 3- zapatos[3]
+                 4- zapatos[4]"""))
         
     
         
