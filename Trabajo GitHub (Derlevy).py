85d7b55f6c90bded9100f9a746565b63d5711950
Derlevy0 <163205452+Derlevy0@users.noreply.github.com>
2024-03-27 17:35:54 -0500

    Guardo los datos y creo los diccionarios

--- Trabajo GitHub (Derlevy).py
+++ Trabajo GitHub (Derlevy).py
@@ -18,6 +18,14 @@ while True:
      break
     except ValueError:
              print("¡Ingrese Solo Valores Numericos!\n")
+DatosComprador.append(Nom,CC,Dir,Tel)
+
+camisetas = {1: ['Polo','Blanca', 15], 2:['Polo','Azul', 15], 3: ['Polo','Roja', 15], 4: ['Polo','Amarilla', 15], 
+             5: ['Cuello redondo','Gris', 12], 6: ['Cuello redondo','Negro', 12], 7: ['Cuello redondo','Verde', 12]}
+           
+jean = {1: ['Azul', 20], 2:['Verde', 20], 3:['Café', 20], 4: ['Negro', 20], 5: ['Gris', 20]}
+
+zapatos = {1:['Botas','Café', 25], 2: ['Tenis','Azul', 20], 3:['Botas','Negro', 25], 4: ['Tenis','Blanco', 20]}
         
     
         
