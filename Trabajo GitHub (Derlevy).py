a34ae7505959874aa02d00f258f9bb3c4c64ab0b
Derlevy0 <163205452+Derlevy0@users.noreply.github.com>
2024-03-27 19:24:52 -0500

    Agrego la validacion try/except y finalmento imprimo la informacion final de las listas

--- Trabajo GitHub (Derlevy).py
+++ Trabajo GitHub (Derlevy).py
@@ -28,32 +28,51 @@ jean = {1: ['Azul', 20], 2:['Verde', 20], 3:['Café', 20], 4: ['Negro', 20], 5: 
 zapatos = {1:['Botas','Café', 25], 2: ['Tenis','Azul', 20], 3:['Botas','Negro', 25], 4: ['Tenis','Blanco', 20]}
 
 while True:
-   P = int(input("""Seleccione una Camiseta:
-             1- camisetas[1]
-             2- camisetas[2]
-             3- camisetas[3]
-             4- camisetas[4]
-             5- camisetas[5]
-             6- camisetas[6]
-             7- camisetas[7]"""))
-   
-   S = int(input("""Seleccione un Jean:
-                 1- jean[1]
-                 2- jean[2]
-                 3- jean[3]
-                 4- jean[4]
-                 5- jean[5]"""))
-                 
-   T = int(input("""seleccione unos Zapatos:
-                 1- zapatos[1]
-                 2- zapatos[2]
-                 3- zapatos[3]
-                 4- zapatos[4]"""))
-    break
+    try:
+     P = int(input("""Seleccione una Camiseta:
+                  1- camisetas[1]
+                  2- camisetas[2]
+                  3- camisetas[3]
+                  4- camisetas[4]
+                  5- camisetas[5]
+                  6- camisetas[6]
+                  7- camisetas[7]\n"""))
+     break
+    except ValueError:
+        print("¡Ingrese Solo Valores Numericos!\n")
+        
+while True:
+    try:
+     S = int(input("""Seleccione un Jean:
+                      1- jean[1]
+                      2- jean[2]
+                      3- jean[3]
+                      4- jean[4]
+                      5- jean[5]\n"""))
+     break
+    except ValueError:
+        print("¡Ingrese Solo Valores Numericos!\n")
+                      
+while True:
+    try:
+     T = int(input("""seleccione unos Zapatos:
+                      1- zapatos[1]
+                      2- zapatos[2]
+                      3- zapatos[3]
+                      4- zapatos[4]\n"""))
+     break
+    except ValueError:
+        print("¡Ingrese Solo Valores Numericos!\n")
 Compras.append(P,S,T)
 
 Total = camisetas[P][2] + jean[S][1] + zapatos[T][2]
 TotalCompras.append(Total)
+
+print(DatosComprador)
+
+print(TotalCompras)
+
+print(Total)
         
     
         
